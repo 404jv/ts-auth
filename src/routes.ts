b66774f4ff0ab99526a6cbe2a15ff50a435ef472
João Victor <joaovictorramalho7@gmail.com>
2021-06-02 20:51:45 -0300

    Create protected route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import AuthController from "./app/controllers/AuthController";
 import UserController from "./app/controllers/UserController";
+import AuthMiddleware from "./app/middleware/authMiddleware";
 
 const routes = Router();
 
+routes.get('/users', AuthMiddleware, UserController.index);
 routes.post('/users', UserController.store);
 routes.post('/auth', AuthController.authenticate);
 
