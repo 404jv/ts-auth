f005b8a9d1e52318a977030cf34190c28e0dd3d0
João Victor <joaovictorramalho7@gmail.com>
2021-06-02 20:13:00 -0300

    Create controller store for user

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import User from '../models/User';
 
 class UserController {
-  store(req: Request, res: Response) {
-    return res.send('OK');  
+  async store(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+      const repository = getRepository(User);
+  
+      const userExists = await repository.findOne({ where: { email } });
+  
+      if (userExists) return res.sendStatus(409);
+  
+      const user = repository.create({ email, password });
+      await repository.save(user);
+  
+      return res.json(user);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
